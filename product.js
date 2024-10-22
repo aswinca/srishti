75a24a1eb63ddf1d42ef0b94bcdac30a2627d7b6
Aswin CA <caaswin2001@gmail.com>
2024-10-22 10:20:39 +0530

    new git

---
+++ product.js
@@ -0,0 +1,12 @@
+function numbers(){
+    for(let i=1;i<10;i++){
+        if(i%2==0){
+            console.log({num});
+            
+        }
+        else{
+            console.log({num});
+            
+        }
+    }
+}
